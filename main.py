0307e924fd5a1a32a00aff5b42eb87e2a97f2086
ReiOle <oserraprof@gmail.com>
2025-11-17 21:28:19 +0100

    item tests pass

---
+++ main.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass, field
+from decimal import Decimal
+from typing import List 
+
+@dataclass
+class Item:
+    id: str
+    name: str
+    price: Decimal
+    quantity: int=1
+    
+    
+class EventBus:
+    def __init__(self):
+        pass
+    
+@dataclass
+class Cart:
+    cart_id:str
+    event_bus: EventBus
+    items: List[Item] = field(default_factory=list)
+    
+    def add_item(self, item:Item):
+        self.items.append(item)
+        
+    def get_items(self):
+        return self.items
