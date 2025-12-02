785d45e5a7131321373ee9e311d2493c951f560b
ReiOle <oserraprof@gmail.com>
2025-12-02 21:44:26 +0100

    acoplamiento

---
+++ teoria_arquitectura_acoplamiento.py
@@ -0,0 +1,5 @@
+# coesion -> habilidad de un elemento de un componente tenga sentido en ese componente 
+# acopladas -> grado de dependencia entre módulos.
+# Lo mejor es que la dependencia sea el minimo possible
+# abstraccion -> grado de capas que tiene mi componente(numero de niveles) Maximo 3 niveles, 4 ya seria demasiado inestable
+# inestabilidad -> como de facil es que mi codigo se rompa (dependencias externas, si se rompen es inestable)
