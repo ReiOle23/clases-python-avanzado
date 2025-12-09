3fc99f1159214d155a29efa851df64c1be317a7b
ReiOle <oserraprof@gmail.com>
2025-12-09 17:52:09 +0100

    .

--- exercice_2.py
+++ exercice_2.py
@@ -20,7 +20,7 @@ class Calculator:
     def multiplicar(n1:Number, n2:Number) -> Number:
         return n1*n2
     
-    staticmethod
+     
     def producto(n1:Number, n2:Number) -> Number:
         return n1/n2
     
@@ -118,3 +118,25 @@ class Perro(AnimalQueCorre, AnimalQueNada):
     
     
 # Dependency Inversion Principle
+from abc import ABC, abstractmethod
+
+class FrontEnd:
+    def __init__(self, data_source):
+        self.data_source = data_source
+
+    def display_data(self):
+        data = self.data_source.get_data()
+        print("Display data:", data)
+
+class DataSource(ABC):
+    @abstractmethod
+    def get_data(self):
+        pass
+
+class Database(DataSource):
+    def get_data(self):
+        return "Data from the database"
+
+class API(DataSource):
+    def get_data(self):
+        return "Data from the API"
